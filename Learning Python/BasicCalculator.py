6592c634fce0669cbd54c0ad5da8fbac77d3efc5
joel goh <joelgoh@joels-MacBook-Air-2.local>
2025-05-31 00:55:06 +1000

    basic calculator done

--- Learning Python/BasicCalculator.py
+++ Learning Python/BasicCalculator.py
@@ -1,3 +1,15 @@
 operator = input("Enter an operator (+ - * /): ")
 
-num1 = int(input("Enter an "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    print(round(num1 + num2, 2))
+elif operator == "-":
+    print(round(num1 - num2, 2))
+elif operator == "*":
+    print(round(num1 * num2, 2))
+elif operator == "/":
+    print(round(num1/num2, 2))
+else:
+    print(f"{operator} is not a valid operator")
