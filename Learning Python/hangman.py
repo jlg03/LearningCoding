088d15cbb7b9115117f2955e4ccdc7c85ff1f70e
joel goh <joelgoh04@gmail.com>
2025-07-05 17:12:00 +0800

    hangman game

--- Learning Python/hangman.py
+++ Learning Python/hangman.py
@@ -24,15 +24,50 @@ hangman_art = {0:(" ",
                   "/|\\",
                   "/\\")}
 
-def display_nam(wrong_guesses):
-    pass
-
+def display_man(wrong_guesses):
+    for line in hangman_art[wrong_guesses]:
+       print(line)
 def display_hint(hint):
-    pass
+    print(" ".join(hint))
 def display_answer(answer):
     pass
 def main():
    answer = random.choice(words)
-   hint = []
+   hint = ["_"] * len(answer)
+   wrong_guesses = 0
+   guessed_letters = set()
+   is_running = True
+   
+   while is_running:
+      display_man(wrong_guesses)
+      display_hint(hint)
+      
+      guess = input("Enter a letter: ").lower()
+      if len(guess) != 1 or not guess.isalpha():
+         print("Invalid input")
+         continue
+      if guess in guessed_letters:
+         print(f"{guess} is already guessed")
+         continue
+      guessed_letters.add(guess)
+      
+      if guess in answer:
+         for i in range(len(answer)):
+            if answer[i] == guess:
+               hint[i] = guess
+      else:
+         wrong_guesses += 1
+      
+      if "_" not in hint:
+         display_man(wrong_guesses)
+         display_answer(answer)
+         print("You Win")
+         is_running = False
+      elif wrong_guesses >= len(hangman_art) - 1:
+         display_man(wrong_guesses)
+         display_answer(answer)
+         print("You Lose")
+         is_running = False
+         
 if __name__ == "__main__":
     main()
