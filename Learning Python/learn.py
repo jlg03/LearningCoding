5ab1637bb875710211b38557774aab535c20c996
joel goh <joelgoh04@gmail.com>
2025-07-11 02:23:35 +0800

    inheritance

--- Learning Python/learn.py
+++ Learning Python/learn.py
@@ -1,17 +1,35 @@
-class Student:
-    
-    class_year = 2024
-    num_students = 0
-    
-    def __init__(self, name, age):
+class Animal:
+    def __init__(self, name):
         self.name = name
-        self.age = age
-        Student.num_students += 1
- 
-student1 = Student("Spongebob", 30)
-student2 = Student("Patrick", 35)
+        self.is_alive = True
+        
+    def eat(self):
+        print(f"{self.name} is eating")
+        
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+        
 
-print(student1.)
-print(student2.name)
+class Dog(Animal):
+    def speak(self):
+        print("Woof")
 
-print(Student.num_students) 
+class Cat(Animal):
+    def speak(self):
+        print("Meow")
+
+class Mouse(Animal):
+    def speak(self):
+        print("Squeak")
+
+dog = Dog("Scooby")
+cat = Cat("Garfield")
+mouse = Mouse("Mickey")
+
+print(dog.name)
+print(dog.is_alive)
+dog.eat()
+dog.sleep()
+dog.speak()
+cat.speak()
+mouse.speak()
