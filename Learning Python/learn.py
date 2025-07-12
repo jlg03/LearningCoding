9b00e42ff793167a241eda70efd3b7d6be9e2b22
joel goh <joelgoh04@gmail.com>
2025-07-12 10:33:14 +0800

    polymorphism

--- Learning Python/learn.py
+++ Learning Python/learn.py
@@ -1,36 +1,22 @@
-from abc import ABC, abstractmethod
-
-class Shape:
+class Animal:
+    alive = True
     
-    @abstractmethod
-    def area(self):
-        pass
-
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-    def area(self):
-        return 3.14 * self.radius ** 2
-
-class Square(Shape):
-    def __init__(self, side):
-        self.side = side
-    def area(self):
-        return self.side ** 2
-
-class Triangle(Shape):
-    def __init__(self, base, height):
-        self.base = base
-        self.height = height
-    def area(self):
-        return self.base * self.height * 0.5
+class Dog(Animal):
+    def speak(self):
+        print("Woof")
+        
+class Cat(Animal):
+    def speak(self):
+        print("Meow")
+        
+class Car:
     
-class Pizza(Circle):
-    def __init__(self, topping, radius):
-        self.topping = topping
-        super().__init__(radius)
+    alive = False
+    def speak(self):
+        print("Honk")
+        
+animals = [Dog(), Cat(), Car()]
 
-shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("pepperoni", 15)]
-
-for shape in shapes:
-    print(shape.area())
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
