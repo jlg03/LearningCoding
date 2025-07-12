a78c4086e9201320fea969c422a7102939702ab1
joel goh <joelgoh04@gmail.com>
2025-07-12 11:04:33 +0800

    magic methods

--- Learning Python/learn.py
+++ Learning Python/learn.py
@@ -1,31 +1,55 @@
-class Student:
-    count = 0
-    total_gpa = 0
-    
-    def __init__(self, name, gpa):
-        self.name = name
-        self.gpa = gpa
-        Student.count += 1
-        Student.total_gpa += gpa
-    
-    #instance method  
-    def get_info(self):
-        return f"{self.name}: {self.gpa}"
-    
-    @classmethod
-    def get_count(cls):
-        return f"Total number of students: {cls.count}"
-    
-    @classmethod
-    def get_gpa(cls):
-        if cls.count == 0:
-            return 0
-        else:
-            return f"Average GPA: {cls.total_gpa/cls.count:.2f}"
-    
+class Book:
+    def __init__(self, title, author, num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
 
-student1 = Student("Spongebob", 3.2)
-student2 = Student("Patrick", 2.0)
-student3 = Student("Sandy", 4.0)
-print(Student.get_count())
-print(Student.get_gpa())
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+    def __eq__(self, other):
+        return self.title == other.title and self.author == other.author
+    
+    def __lt__(self, other):
+        return self.num_pages < other.num_pages
+    
+    def __gt__(self, other):
+        return self.num_pages > other.num_pages
+    
+    def __add__(self, other):
+        return self.num_pages + other.num_pages
+    
+    def __contains__(self, keyword):
+        return keyword in self.title or keyword in self.author
+    
+    def __getitem__(self, key):
+        if key == "title":
+            return self.title
+        elif key == "author":
+            return self.author
+        elif key == "num_pages":
+            return self.num_pages
+        
+
+
+
+book1 = Book("The Hobbit", "J.R.R. Tolkien", 310)
+book2 = Book("Harry Potter and The Philosophers Stone", "J.K Rowling", 223)
+book3 = Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 172)
+
+print(book1)
+print(book2)
+print(book3)
+
+print(book2 > book3)
+
+print(book2 + book3)
+
+print("Lion" in book3)
+print("Lion" in book1)
+
+print(book1['title'])
+print(book1["author"])
+print(book1["num_pages"])
+
+        
