cd0918de2b0d8d5078d3de02b3c9ecaf1db0d15f
joel goh <joelgoh04@gmail.com>
2025-07-12 11:37:03 +0800

    decorators

--- Learning Python/learn.py
+++ Learning Python/learn.py
@@ -1,47 +1,18 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
-    
-    @property
-    def width(self):
-        return f"{self._width:.1f}cm"
-    
-    @property
-    def height(self):
-        return f"{self._height:.1f}cm"
+def add_sprinkles(func):
+    def wrapper(*args, **kwargs):
+        print("You add sprinkles")
+        func(*args, **kwargs)
+    return wrapper
 
-    @width.setter
-    def width(self, new_width):
-        if new_width > 0:
-            self._width = new_width
-        else:
-            print("Width must be greater then 0")
-            
-    @height.setter
-    def height(self, new_height):
-        if new_height > 0:
-            self._height = new_height
-        else:
-            print("Height must be greater then 0")
-        
-    @width.deleter
-    def width(self):
-        del self._width
-        print("Width deleted") 
-        
-    @height.deleter
-    def height(self):
-        del self._height
-        print("Height deleted")  
-    
-    
-        
-rectangle = Rectangle(3, 4)
+def add_fudge(func):
+    def wrapper(*args, **kwargs):
+        print("You add fudge")
+        func(*args, **kwargs)
+    return wrapper    
 
-rectangle.height = 0
-rectangle.width = 10
-rectangle.height = 5
-
-del rectangle.width
-del rectangle.height
+@add_sprinkles 
+@add_fudge
+def get_icecream(flavour):
+     print(f"Here is your ice {flavour} cream")
+     
+get_icecream("Vanilla")
