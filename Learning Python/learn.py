20edfbddcd3ee37eadce1ec145f3966b196695a3
joel goh <joelgoh04@gmail.com>
2025-07-14 01:54:00 +0800

    csv file python

--- Learning Python/learn.py
+++ Learning Python/learn.py
@@ -1,15 +1,18 @@
-import json
+import csv
 
-employee = {
-    "name": "Spongebob",
-    "age": 30,
-    "job": "cook"
-}
+employees = [["Name", "Age", "Job"],
+             ["Spongebob", 30, "Cook"],
+             ["Patrick", 37, "Unemployed"],
+             ["Sandy", 27, "Scientist"]]
 
-file_path = "output.json"
+file_path = "output.csv"
+
+
 try:
     with open(file_path, "w") as file:
-        json.dump(employee, file, indent=4)
-        print(f"json file '{file_path}' was created")
+        writer = csv.writer(file)
+        for row in employees:
+            writer.writerow(row)
+        print(f"csv file '{file_path}' was created")
 except FileExistsError:
     print("That file already exists ")
