16665e4b843fb5f729dc7b33eb04f7f9ff2f4596
joel goh <joelgoh04@gmail.com>
2025-07-15 18:20:19 +0800

    line edit pyat5

--- Learning Python/learn.py
+++ Learning Python/learn.py
@@ -1,48 +1,31 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QRadioButton, QButtonGroup
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(0, 0, 500, 500)
-        self.radio1 = QRadioButton("Visa", self)
-        self.radio2 = QRadioButton("Mastercard", self)
-        self.radio3 = QRadioButton("Gift Card", self)
-        self.radio4 = QRadioButton("In-Store", self)
-        self.radio5 = QRadioButton("Online", self)
-        self.button_group1 = QButtonGroup(self)
-        self.button_group2 = QButtonGroup(self)
-
+        self.line_edit = QLineEdit(self)
+        self.button = QPushButton("Submit", self)
         self.initUI()
         
     def initUI(self):
-        self.radio1.setGeometry(0, 0, 300, 50)
-        self.radio2.setGeometry(0, 50, 300, 50)
-        self.radio3.setGeometry(0, 100, 300, 50)
-        self.radio4.setGeometry(0, 150, 300, 50)
-        self.radio5.setGeometry(0, 200, 300, 50)
-        self.setStyleSheet("QRadioButton{"
-                           "font-size: 40px;"
-                           "font-family: Arial;"
-                           "padding: 10px;"
-                           "}")
-        self.button_group1.addButton(self.radio1)
-        self.button_group1.addButton(self.radio2)
-        self.button_group1.addButton(self.radio3)
-        self.button_group2.addButton(self.radio4)
-        self.button_group2.addButton(self.radio5)
+        self.line_edit.setGeometry(10, 10, 200, 40)
+        self.line_edit.setStyleSheet("font-size: 25px;"
+                                     "font-family: Arial;")
+        self.button.setGeometry(210, 10, 100, 40)
+        self.button.setStyleSheet("font-size: 25px;")
         
-        self.radio1.toggled.connect(self.radio_button_changed)
-        self.radio2.toggled.connect(self.radio_button_changed)
-        self.radio3.toggled.connect(self.radio_button_changed)
-        self.radio4.toggled.connect(self.radio_button_changed)
-        self.radio5.toggled.connect(self.radio_button_changed)
+        self.line_edit.setPlaceholderText("Enter your name")
 
-    def radio_button_changed(self):
-        radio_button = self.sender()
-        if radio_button.isChecked():
-            print(f"{radio_button.text()} is selected")
+        self.button.clicked.connect(self.submit)
+        
+    def submit(self):
+        text = self.line_edit.text()
+        print(f"Hello {text}")
+        
+        
 
 
 def main():
