2087f632d9c53ceff7f32f7b11cf4b507cd2416b
joel goh <joelgoh04@gmail.com>
2025-07-15 17:48:25 +0800

    radio buttons pyqt5

--- Learning Python/learn.py
+++ Learning Python/learn.py
@@ -1,28 +1,49 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QRadioButton, QButtonGroup
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(0, 0, 500, 500)
-        self.checkbox = QCheckBox("Do you like food?", self)
+        self.radio1 = QRadioButton("Visa", self)
+        self.radio2 = QRadioButton("Mastercard", self)
+        self.radio3 = QRadioButton("Gift Card", self)
+        self.radio4 = QRadioButton("In-Store", self)
+        self.radio5 = QRadioButton("Online", self)
+        self.button_group1 = QButtonGroup(self)
+        self.button_group2 = QButtonGroup(self)
+
         self.initUI()
         
     def initUI(self):
-        self.checkbox.setStyleSheet("font-size: 30px;"
-                                    "font-family: Arial;")
-        self.checkbox.setGeometry(10, 0, 500, 100)
-        self.checkbox.setChecked(False)
+        self.radio1.setGeometry(0, 0, 300, 50)
+        self.radio2.setGeometry(0, 50, 300, 50)
+        self.radio3.setGeometry(0, 100, 300, 50)
+        self.radio4.setGeometry(0, 150, 300, 50)
+        self.radio5.setGeometry(0, 200, 300, 50)
+        self.setStyleSheet("QRadioButton{"
+                           "font-size: 40px;"
+                           "font-family: Arial;"
+                           "padding: 10px;"
+                           "}")
+        self.button_group1.addButton(self.radio1)
+        self.button_group1.addButton(self.radio2)
+        self.button_group1.addButton(self.radio3)
+        self.button_group2.addButton(self.radio4)
+        self.button_group2.addButton(self.radio5)
         
-        self.checkbox.stateChanged.connect(self.checkbox_changed)
-        
-    def checkbox_changed(self, state):
-        if state == Qt.Checked:
-            print("You like food")
-        else:
-            print("You do not like food")
+        self.radio1.toggled.connect(self.radio_button_changed)
+        self.radio2.toggled.connect(self.radio_button_changed)
+        self.radio3.toggled.connect(self.radio_button_changed)
+        self.radio4.toggled.connect(self.radio_button_changed)
+        self.radio5.toggled.connect(self.radio_button_changed)
+
+    def radio_button_changed(self):
+        radio_button = self.sender()
+        if radio_button.isChecked():
+            print(f"{radio_button.text()} is selected")
+
 
 def main():
     app = QApplication(sys.argv)
