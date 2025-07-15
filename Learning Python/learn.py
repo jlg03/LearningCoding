2ad25259398b33feb413c42af957ff6844c9dbc0
joel goh <joelgoh04@gmail.com>
2025-07-15 15:46:10 +0800

    gui labels python

--- Learning Python/learn.py
+++ Learning Python/learn.py
@@ -1,13 +1,39 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My First GUI")
         self.setGeometry(0, 0, 500, 500)
-        # self.setWindowIcon(QIcon("filepath"))
+        
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: blue;"
+                            "background-color: red;"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+        # label.setAlignment(Qt.AlignTop)
+        # label.setAlignment(Qt.AlignBottom)
+        # label.setAlignment(Qt.AlignVCenter)
+        # label.setAlignment(Qt.AlignRight)
+        # label.setAlignment(Qt.AlignTop)
+        # label.setAlignment(Qt.AlignHCenter)
+        # label.setAlignment(Qt.AlignLeft)
+        
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        label.setAlignment(Qt.AlignCenter)
+
+
+
+
+        
         
 def main():
     app = QApplication(sys.argv)
