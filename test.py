dd4692332eaaede1d5ab2dcfaef1aa476fcf2465
joel goh <joelgoh04@gmail.com>
2025-09-15 14:24:55 +1000

    restart project

--- test.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, date
-
-today = date.today()
-dat = datetime.strptime("2025-09-04", "%Y-%m-%d")
-print(type(date))
-print(type(dat))
-print(dat.date() == today)
-print(type(dat))
